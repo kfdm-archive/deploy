ce9ccab825d4caa58a80f22b5a4fa60c40afdb71
Paul Traylor <paul@wegame.com>
2010-11-23 02:44:30 -0500

    Read GitHub post information and send ref and hash to deploy script

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 get '/' do
 	'Deploy Server is running'
@@ -17,7 +18,19 @@ def deploy(name)
 	script = File.dirname(File.expand_path(__FILE__))+"/scripts/deploy_#{name}"
 	return [400,'Missing Deploy Script'] if not File.exist?(script)
 	return [400,'Invalid Deploy Script'] if not File.executable?(script)
-	result = system(script)
+	
+	begin # Parse GitHub Payload
+	  push = JSON.parse(params[:payload])
+	  puts "Running #{script}"
+	  puts "  Reference #{push['ref']}"
+	  puts "  Hash #{push['after']}"
+	  result = system("#{script} #{push['ref']} #{push['after']}")
+	  puts result
+	rescue
+	  puts "Running #{script}"
+	  result = system(script)
+	  puts result
+  end
 	result
 end
 
